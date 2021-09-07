41e5ebb5bcf518a8dbfdc62ae71fe516e82c720c
Naggggyung <74478432+nagggyung@users.noreply.github.com>
2021-09-07 22:53:36 +0900

    Create PyTorch에서 이미지 열기.py

---
+++ 인공지능/PIL/PyTorch에서 이미지 열기.py
@@ -0,0 +1,17 @@
+import PIL
+import torch
+import torchvision.transforms as transforms
+
+img = PIL.Image.open('MobileFaceNet_Tutorial_Pytorch/images/nagyung1.jpg')
+# img.show()
+
+tf = transforms.ToTensor() # torch tensor로 변환
+img_t = tf(img)
+print(img_t.size()) # channel, height, width
+
+tf = transforms.ToPILImage() # Tensor에서 PIL로 변환
+img_t = tf(img_t)
+print(img_t) # <PIL.Image.Image image mode=RGB size=1080x1440 at 0x1F825089A00>
+
+img_t.show()
+
